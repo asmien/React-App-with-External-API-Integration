037892df4e37fc63a434c7fe61249218043376d6
Sharon <sharonapot@gmail.com>
2026-05-06 08:21:04 +0300

    added navbar and search hero structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Tukio Hub</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SearchHero.jsx
@@ -0,0 +1,9 @@
+const SearchHero = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search events..." />
+    </div>
+  );
+};
+
+export default SearchHero;
